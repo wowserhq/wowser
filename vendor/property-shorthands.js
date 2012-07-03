11be3e3135428bc7d63f5aa81d4a7da2d0229312
Tim Kurvers <tim@moonsphere.net>
2012-07-03 21:24:03 +0200

    Fixed getter/setters

--- vendor/property-shorthands.js
+++ vendor/property-shorthands.js
@@ -1,4 +1,8 @@
 
 // Alias convenience prototype methods to deal with getter/setters
-Function.prototype.getter = Function.prototype.__defineGetter__;
-Function.prototype.setter = Function.prototype.__defineSetter__;
+Function.prototype.getter = function(name, getter) {
+  this.prototype.__defineGetter__(name, getter);
+};
+Function.prototype.setter = function(name, setter) {
+  this.prototype.__defineSetter__(name, setter);
+};
