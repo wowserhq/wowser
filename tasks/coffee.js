10896238bbf0b135abf6f061a63f64d957038d79
Tim Kurvers <tim@moonsphere.net>
2012-07-17 02:07:47 +0200

    Updated CoffeeScript compile task

--- tasks/coffee.js
+++ tasks/coffee.js
@@ -4,40 +4,24 @@
  * 
  * See: http://takazudo.github.com/blog/entry/2012-04-14-grunt-coffee.html
  */
+
 module.exports = function(grunt) {
   
-  var log = grunt.log;
- 
   var exec = require('child_process').exec;
   
-  grunt.registerHelper('exec', function(opts, done) {
-    var command = opts.cmd + ' ' + opts.args.join(' ');
-    exec(command, opts.opts, function(code, stdout, stderr) {
-      if(!done) {
-        return;
-      }
-      if(code === 0) {
-        done(null, stdout, code);
-      }else{
-        done(code, stderr, code);
-      }
-    });
-  });
+  var log = grunt.log;
   
   grunt.registerMultiTask('coffee', 'compiles CoffeeScript files.', function() {
     var done = this.async();
     var dir = this.data.dir;
     var dest = this.data.dest;
     
-    var args = {
-      cmd: 'coffee',
-      args: ['--bare', '--compile', '--output', dest, dir ]
-    };
+    var cmd = 'coffee --bare --compile --output ' + dest + ' ' + dir;
     
-    grunt.helper('exec', args, function(err, stdout, code) {
-      if(err) {
+    exec(cmd, [], function(error, stdout, stderr) {
+      if(error !== null) {
         log.writeln(dir + ': failed to compile to ' + dest);
-        log.writeln(stdout);
+        log.writeln(stderr);
         done(false);
       }else{
         log.writeln(dir + ': compiled to ' + dest);
@@ -45,5 +29,5 @@ module.exports = function(grunt) {
       }
     });
   });
- 
+
 };
