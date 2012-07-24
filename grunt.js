8562f346ff7d42149fee4284b6f2fb39f611a8db
Tim Kurvers <tim@moonsphere.net>
2012-07-24 21:30:16 +0200

    Included entities package in build procedure

--- grunt.js
+++ grunt.js
@@ -58,6 +58,7 @@ module.exports = function(grunt) {
           'build/wrathnet/expansions/wotlk/enums/**/*.js',
           'build/wrathnet/expansions/wotlk/net/**/*.js',
           'build/wrathnet/expansions/wotlk/**/*.js',
+          'build/wrathnet/entities/**/*.js',
           'build/wrathnet/sessions/**/*.js',
           'vendor/**/*.js'
         ],
