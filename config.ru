47f6f25351659acfa144472523af639fd31609c5
Tim Kurvers <tim@moonsphere.net>
2014-04-24 22:00:30 +0200

    Refactor client into stand-alone Sinatra app

--- config.ru
+++ config.ru
@@ -2,4 +2,7 @@ lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'wowser'
-run Wowser
+
+map '/' do
+  run Wowser::Client
+end
---
+++ lib/wowser/client.rb
@@ -0,0 +1,31 @@
+require 'sinatra/asset_pipeline'
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Wowser::Client < Sinatra::Base
+
+  configure do
+    set :root, File.dirname(__FILE__) + '/../../'
+
+    register Sinatra::AssetPipeline
+
+    # Locate vendor assets installed through Bower
+    sprockets.append_path File.join(root, 'vendor', 'assets')
+
+    # Disable CoffeeScript's anonymous function wrap
+    Tilt::CoffeeScriptTemplate.default_bare = true
+  end
+
+  configure :development do
+    register Sinatra::Reloader
+
+    # Configure Sprockets' caching mechanism manually as it defaults to none
+    # See: https://github.com/middleman/middleman-sprockets/pull/25
+    sprockets.cache = {}
+  end
+
+  get '/' do
+    erb :index
+  end
+
+end
