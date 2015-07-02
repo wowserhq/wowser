481261179c27037aae86d6c7988ab398b5b5814e
Tim Kurvers <tim@moonsphere.net>
2015-07-02 20:10:17 +0200

    Remove compiled code

--- spec/sample-spec.js
+++
@@ -1,7 +0,0 @@
-var expect, sinon, _ref;
-
-_ref = require('./spec-helper'), expect = _ref.expect, sinon = _ref.sinon;
-
-describe('Wowser', function() {
-  return xit('will have specs (hopefully)');
-});
