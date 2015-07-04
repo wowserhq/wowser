15026f9c963e32a6fa44272df030f057b4fd48c1
Tim Kurvers <tim@moonsphere.net>
2015-07-04 22:04:32 +0200

    Fix ObjectUtil.keyByValue

--- src/scripts/lib/client/utils/object-util.js
+++ src/scripts/lib/client/utils/object-util.js
@@ -2,11 +2,12 @@ module.exports = class ObjectUtil {
 
   // Retrieves key for given value (if any) in object
   static keyByValue(object, target) {
-    if(!'lookup' in object) {
-      lookup = {}
+    if(!('lookup' in object)) {
+      const lookup = {}
       for(var key in object) {
         if(object.hasOwnProperty(key)) {
-          lookup[value] = object[key]
+          var value = object[key]
+          lookup[value] = key
         }
       }
       object.lookup = lookup
