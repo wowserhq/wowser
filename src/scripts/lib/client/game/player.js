3bc255f2d83b09a967c51127ba046df162d9f7e8
Tim Kurvers <tim@moonsphere.net>
2015-07-26 13:34:54 +0200

    Add preliminary Unit and Player

---
+++ src/scripts/lib/client/game/player.js
@@ -0,0 +1,16 @@
+const Unit = require('./unit');
+
+module.exports = class Player extends Unit {
+
+  constructor() {
+    super();
+
+    this.name = 'Player';
+    this.hp = this.hp;
+    this.mp = this.mp;
+
+    this.target = new Unit();
+    this.target.name = 'Illidan';
+  }
+
+};
---
+++ src/scripts/lib/client/game/unit.js
@@ -0,0 +1,19 @@
+module.exports = class Unit {
+
+  constructor() {
+    this.name = '<unknown>';
+    this.level = '?';
+    this.target = null;
+
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+
+    this.maxHp = 0;
+    this.hp = 0;
+
+    this.maxMp = 0;
+    this.mp = 0;
+  }
+
+};
