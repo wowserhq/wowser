c22222cfc2b8b695520275e7335a5d31cb3df6ee
Tim Kurvers <tim@moonsphere.net>
2015-08-19 00:00:54 +0200

    Update world handler accordingly

--- src/lib/game/world/handler.js
+++ src/lib/game/world/handler.js
@@ -13,34 +13,29 @@ module.exports = class WorldHandler extends EventEmitter {
 
     M2.load('Creature\\Rabbit\\Rabbit.m2').then((m2) => {
       m2.texture = 'Creature\\Rabbit\\RabbitSkin.blp.png';
-      const model = m2.mesh;
-      model.position.x = 2;
-      model.position.y = -1;
-      this.scene.add(model);
+      m2.position.x = 2;
+      m2.position.y = -1;
+      this.scene.add(m2);
     });
 
     M2.load('Creature\\Illidan\\Illidan.m2').then((m2) => {
       m2.texture = 'Creature\\Illidan\\Illidan.blp.png';
-      const model = m2.mesh;
-      this.character = model;
-      this.scene.add(model);
+      this.scene.add(m2);
     });
 
     M2.load('Creature\\RAGNAROS\\RAGNAROS.m2').then((m2) => {
       m2.texture = 'Creature\\RAGNAROS\\RAGNAROSSKIN.blp.png';
-      const model = m2.mesh;
-      model.position.x = -5;
-      model.position.y = 5.5;
-      model.scale.set(0.3, 0.3, 0.3);
-      this.scene.add(model);
+      m2.position.x = -5;
+      m2.position.y = 5.5;
+      m2.scale.set(0.3, 0.3, 0.3);
+      this.scene.add(m2);
     });
 
     M2.load('Creature\\MurlocCostume\\murloccostume_whiteflag.M2').then((m2) => {
       m2.texture = 'Creature\\MurlocCostume\\MURLOCCOSTUME.blp.png';
-      const model = m2.mesh;
-      model.position.x = 2;
-      model.position.y = 1.5;
-      this.scene.add(model);
+      m2.position.x = 2;
+      m2.position.y = 1.5;
+      this.scene.add(m2);
     });
 
     ADT.load('World\\Maps\\Azeroth\\Azeroth_31_49.adt').then((adt) => {
