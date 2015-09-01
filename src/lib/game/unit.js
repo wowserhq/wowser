92918cc577cf58793a3c7952687f1801d2be1256
Tim Kurvers <tim@moonsphere.net>
2015-09-01 23:49:46 +0200

    Allow setting displayID for units

--- src/lib/game/unit.js
+++ src/lib/game/unit.js
@@ -1,4 +1,6 @@
+const DBC = require('../pipeline/dbc');
 const Entity = require('./entity');
+const M2 = require('../pipeline/m2');
 const THREE = require('three');
 
 module.exports = class Unit extends Entity {
@@ -20,6 +22,34 @@ module.exports = class Unit extends Entity {
 
     this.displayID = 0;
     this.model = null;
+  }
+
+  get texturePath() {
+    const info = this.displayInfo;
+
+    // TODO: This does not seem to work for all models
+    const skin = info.skin1 || info.skin2 || info.skin3;
+    return `${this.modelData.path}${skin}.blp`;
+  }
+
+  set displayID(displayID) {
+    DBC.load('CreatureDisplayInfo', displayID).then((displayInfo) => {
+      this.displayInfo = displayInfo;
+      const {modelID} = displayInfo;
+
+      DBC.load('CreatureModelData', modelID).then((modelData) => {
+        this.modelData = modelData;
+        this.modelData.path = this.modelData.file.match(/^(.+?)(?:[^\\]+)$/)[1];
+        this.modelData.file = this.modelData.file.replace('.mdx', '.m2');
+
+        M2.load(this.modelData.file).then((m2) => {
+          // TODO: Clone M2 instance to avoid modifying cached version
+          this.emit('change:model', this.model, m2);
+          this.model = m2;
+          this.model.texture = this.texturePath;
+        });
+      });
+    });
   }
 
 };
