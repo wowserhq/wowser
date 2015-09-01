5f79a3f01ae22ec30f3a3a335c92a46a04bd4e4b
Tim Kurvers <tim@moonsphere.net>
2015-09-01 23:49:09 +0200

    Allow entities to emit events

--- src/lib/game/entity.js
+++ src/lib/game/entity.js
@@ -1,6 +1,9 @@
-module.exports = class Entity {
+const {EventEmitter} = require('events');
+
+module.exports = class Entity extends EventEmitter {
 
   constructor() {
+    super();
     this.guid = Math.random() * 1000000 | 0;
   }
 
