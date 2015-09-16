ee59dd110f2de088262f9f5c042936fbe4d8242f
Tim Kurvers <tim@moonsphere.net>
2015-09-16 22:13:37 +0200

    Convenience worldport method

--- src/lib/game/world/handler.js
+++ src/lib/game/world/handler.js
@@ -9,13 +9,12 @@ module.exports = class WorldHandler extends EventEmitter {
   constructor(session) {
     super();
     this.session = session;
+    this.player = this.session.player;
 
     this.scene = new THREE.Scene();
 
-    const player = this.session.player;
-
     // TODO: Use this handler for all entities, not just the player
-    player.on('change:model', (oldModel, newModel) => {
+    this.player.on('change:model', (oldModel, newModel) => {
       if (oldModel) {
         this.scene.remove(oldModel);
       }
@@ -23,51 +22,74 @@ module.exports = class WorldHandler extends EventEmitter {
       this.scene.add(newModel);
     });
 
-    player.position.set(5100, 740, 0);
+    // Darkshire
+    this.worldport('Azeroth', -10559, -1189, 28);
 
-    ADT.loadAtCoords('Kalimdor', 5100, 740).then((adt) => {
-      this.scene.add(adt);
-    });
+    // Darnassus (Kalimdor)
+    // this.worldport('Kalimdor', 9947, 2557, 1316);
+  }
 
-    M2.load('Creature\\Rabbit\\Rabbit.m2').then((m2) => {
-      m2.texture = 'Creature\\Rabbit\\RabbitSkin.blp';
-      m2.position.x = 2;
-      m2.position.y = -1;
-      this.scene.add(m2);
-    });
+  worldport(map, x, y, z) {
+    this.player.position.set(x, y, z);
 
-    M2.load('Creature\\Illidan\\Illidan.m2').then((m2) => {
-      m2.texture = 'Creature\\Illidan\\Illidan.blp';
-      m2.position.x = -3;
-      m2.position.y = -1.5;
-      this.scene.add(m2);
-    });
-
-    M2.load('Creature\\RAGNAROS\\RAGNAROS.m2').then((m2) => {
-      m2.texture = 'Creature\\RAGNAROS\\RAGNAROSSKIN.blp';
-      m2.position.x = -5;
-      m2.position.y = 5.5;
-      m2.scale.set(0.3, 0.3, 0.3);
-      this.scene.add(m2);
-    });
-
-    M2.load('Creature\\MurlocCostume\\murloccostume_whiteflag.M2').then((m2) => {
-      m2.texture = 'Creature\\MurlocCostume\\MURLOCCOSTUME.blp';
-      m2.position.x = 2;
-      m2.position.y = 1.5;
-      this.scene.add(m2);
-    });
-
-    WMO.load('WORLD\\WMO\\AZEROTH\\COLLIDABLE DOODADS\\ELWYNN\\ABBEYGATE\\ABBEYGATE01.WMO').then((wmo) => {
-      this.scene.add(wmo);
-    });
-
-    ADT.load('World\\Maps\\Azeroth\\Azeroth_31_49.adt').then((adt) => {
-      adt.position.x = -266.667;
-      adt.position.y = -266.667;
-      adt.position.z = -67;
+    ADT.loadAtCoords(map, x, y).then((adt) => {
       this.scene.add(adt);
     });
   }
+
+  // M2.load('Creature\\Rabbit\\Rabbit.m2').then((m2) => {
+  //   m2.texture = 'Creature\\Rabbit\\RabbitSkin.blp';
+  //   m2.position.x = 2;
+  //   m2.position.y = -1;
+  //   this.scene.add(m2);
+  // });
+
+  // M2.load('Creature\\RAGNAROS\\RAGNAROS.m2').then((m2) => {
+  // M2.load('Creature\\Illidan\\Illidan.m2').then((m2) => {
+  // // M2.load('Creature\\NorthrendPenguin\\NorthrendPenguin.m2').then((m2) => {
+  //   m2.texture = 'Creature\\Illidan\\Illidan.blp';
+
+  //   const helper = new THREE.SkeletonHelper(m2);
+  //   helper.material.linewidth = 2;
+  //   // helper.visible = false;
+  //   this.scene.add(helper);
+
+  //   this.scene.add(m2);
+  // });
+
+  // M2.load('Creature\\Illidan\\Illidan.m2').then((m2) => {
+  //   m2.texture = 'Creature\\Illidan\\Illidan.blp';
+  //   m2.position.x = -3;
+  //   m2.position.y = -1.5;
+  //   this.scene.add(m2);
+  // });
+
+  // M2.load('Creature\\RAGNAROS\\RAGNAROS.m2').then((m2) => {
+  //   m2.texture = 'Creature\\RAGNAROS\\RAGNAROSSKIN.blp';
+  //   m2.position.x = -5;
+  //   m2.position.y = 5.5;
+  //   m2.scale.set(0.3, 0.3, 0.3);
+  //   this.scene.add(m2);
+  // });
+
+  // M2.load('Creature\\MurlocCostume\\murloccostume_whiteflag.M2').then((m2) => {
+  //   m2.texture = 'Creature\\MurlocCostume\\MURLOCCOSTUME.blp';
+  //   m2.position.x = 2;
+  //   m2.position.y = 1.5;
+  //   this.scene.add(m2);
+  // });
+
+  // WMO.load('World\\wmo\\Azeroth\\Buildings\\Stormwind\\Stormwind.wmo').then((wmo) => {
+  // WMO.load('World\\wmo\\KhazModan\\Cities\\Ironforge\\ironforge.wmo').then((wmo) => {
+  // WMO.load('WORLD\\WMO\\AZEROTH\\COLLIDABLE DOODADS\\ELWYNN\\ABBEYGATE\\ABBEYGATE01.WMO').then((wmo) => {
+  //   this.scene.add(wmo);
+  // });
+
+  // ADT.load('World\\Maps\\Azeroth\\Azeroth_31_49.adt').then((adt) => {
+  //   adt.position.x = -266.667;
+  //   adt.position.y = -266.667;
+  //   adt.position.z = -67;
+  //   this.scene.add(adt);
+  // });
 
 };
