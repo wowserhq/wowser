f0f0edab9c724b3806633e073e97163683acd64d
Tim Kurvers <tim@moonsphere.net>
2015-11-19 22:39:16 +0100

    Initial webpack configuration

--- src/lib/ui/index.js
+++
@@ -1,7 +0,0 @@
-const riot = require('riot');
-
-require('../../src/ui/templates/**/*.html', { mode: 'expand' });
-
-document.addEventListener('DOMContentLoaded', function() {
-  riot.mount('*');
-});
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname + '/src',
+  entry: './bootstrapper',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: true,
+      template: 'src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: __dirname + '/public'
+  }
+};
