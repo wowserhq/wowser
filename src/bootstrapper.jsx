b393f8351fc5a0d13ddb9e4c154909e9adb36dbc
Tim Kurvers <tim@moonsphere.net>
2015-11-19 23:21:21 +0100

    Add React

---
+++ src/bootstrapper.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const Wowser = require('./components/wowser');
+
+ReactDOM.render(<Wowser />, document.querySelector('app'));
---
+++ src/components/wowser/index.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Wowser extends React.Component {
+
+  render() {
+    return (
+      <wowser></wowser>
+    );
+  }
+
+}
+
+export default Wowser;
