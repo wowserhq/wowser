12d50f5ea96bcb2664afae4e70136c17df0baa67
Tim Kurvers <tim@moonsphere.net>
2015-11-23 22:51:39 +0100

    Add main Wowser component and UI

--- src/components/wowser/index.jsx
+++ src/components/wowser/index.jsx
@@ -1,13 +1,78 @@
 const React = require('react');
 
+const AuthScreen = require('../auth');
+const CharactersScreen = require('../characters');
+const GameScreen = require('../game');
+const RealmsScreen = require('../realms');
+const Kit = require('../kit');
+const session = require('./session');
+
+require('./index.styl');
+
 class Wowser extends React.Component {
 
+  static SCREENS = [
+    AuthScreen,
+    RealmsScreen,
+    CharactersScreen,
+    GameScreen,
+    Kit
+  ];
+
+  constructor() {
+    super();
+
+    this.state = {
+      screen: session.screen
+    };
+
+    this._onScreenChange = this._onScreenChange.bind(this);
+
+    session.on('screen:change', this._onScreenChange);
+  }
+
+  get currentScreen() {
+    const Screen = this.constructor.SCREENS.find((screen) => {
+      return screen.id === this.state.screen;
+    });
+    return <Screen />;
+  }
+
+  _onScreenChange(from, to) {
+    this.setState({ screen: to });
+  }
+
+  _onScreenSelect(event) {
+    session.screen = event.target.value;
+  }
+
   render() {
+    const screens = this.constructor.SCREENS;
     return (
-      <wowser></wowser>
+      <wowser>
+        <div className="branding">
+          <header>Wowser</header>
+          <div className="divider"></div>
+          <div className="slogan">World of Warcraft in the browser</div>
+        </div>
+
+        <select className="screen-selector"
+                value={ this.state.screen }
+                onChange={ this._onScreenSelect.bind(this) }>
+          { screens.map((screen) => {
+            return (
+              <option key={ screen.id } value={ screen.id }>
+                { screen.title }
+              </option>
+            );
+          }) }
+        </select>
+
+        { this.currentScreen }
+      </wowser>
     );
   }
 
 }
 
-export default Wowser;
+module.exports = Wowser;
