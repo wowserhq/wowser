00f8c75b23e58c83d32f4279aad79f6d6713cc96
Tim Kurvers <tim@moonsphere.net>
2015-11-23 22:48:01 +0100

    Expose Session to manage screens

---
+++ src/components/wowser/session.js
@@ -0,0 +1,24 @@
+const Client = require('../../lib');
+
+class Session extends Client {
+
+  constructor() {
+    super();
+
+    this._screen = 'auth';
+  }
+
+  get screen() {
+    return this._screen;
+  }
+
+  set screen(screen) {
+    if (this._screen !== screen) {
+      this.emit('screen:change', this._screen, screen);
+      this._screen = screen;
+    }
+  }
+
+}
+
+module.exports = new Session();
