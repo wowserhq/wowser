5702f767a4741cff02b525e95f6cc5aa1caf9148
Tim Kurvers <tim@moonsphere.net>
2015-11-27 21:38:57 +0100

    Add portrait component

---
+++ src/components/game/portrait/index.jsx
@@ -0,0 +1,37 @@
+const React = require('react');
+const classes = require('classnames');
+
+require('./index.styl');
+
+class Portrait extends React.Component {
+
+  static propTypes = {
+    self: React.PropTypes.bool,
+    unit: React.PropTypes.object.isRequired,
+    target: React.PropTypes.bool
+  };
+
+  render() {
+    const unit = this.props.unit;
+    const className = classes('portrait', {
+      self: this.props.self,
+      target: this.props.target
+    });
+    return (
+      <portrait className={ className }>
+        <div className="icon portrait"></div>
+
+        <header className="name">{ unit.name }</header>
+        <aside className="level">{ unit.level }</aside>
+
+        <div className="divider"></div>
+
+        <div className="health">{ unit.hp } / { unit.maxHp }</div>
+        <div className="mana">{ unit.mp } / { unit.maxMp }</div>
+      </portrait>
+    );
+  }
+
+}
+
+module.exports = Portrait;
