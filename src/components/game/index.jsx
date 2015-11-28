2b0d51762a8b618b6b812b360d2e6b0320fb749b
Tim Kurvers <tim@moonsphere.net>
2015-11-28 11:29:12 +0100

    Finalize game screen

--- src/components/game/index.jsx
+++ src/components/game/index.jsx
@@ -1,4 +1,10 @@
 const React = require('react');
+const THREE = require('three');
+
+require('./index.styl');
+
+const HUD = require('./hud');
+const session = require('../wowser/session');
 
 class GameScreen extends React.Component {
 
@@ -7,10 +13,49 @@ class GameScreen extends React.Component {
 
   constructor() {
     super();
+
+    this.animate = ::this.animate;
+    this.resize = ::this.resize;
+
+    this.camera = new THREE.PerspectiveCamera(60, this.aspectRatio, 1, 1000);
+    this.camera.up.set(0, 0, 1);
+    this.camera.position.set(15, 0, 7);
+  }
+
+  componentDidMount() {
+    this.renderer = new THREE.WebGLRenderer({
+      alpha: true,
+      canvas: this.refs.canvas
+    });
+
+    this.resize();
+    this.animate();
+
+    window.addEventListener('resize', this.resize);
+  }
+
+  get aspectRatio() {
+    return window.innerWidth / window.innerHeight;
+  }
+
+  resize() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.aspect = this.aspectRatio;
+    this.camera.updateProjectionMatrix();
+  }
+
+  animate() {
+    this.renderer.render(session.world.scene, this.camera);
+    requestAnimationFrame(this.animate);
   }
 
   render() {
-    return <game></game>;
+    return (
+      <game className="game screen">
+        <canvas ref="canvas"></canvas>
+        <HUD />
+      </game>
+    );
   }
 
 }
