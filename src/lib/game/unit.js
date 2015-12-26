c6f8e314907c08c16e5b97235f8511ae5362d8e7
Tim Kurvers <tim@moonsphere.net>
2015-12-26 21:17:51 +0100

    Refactor unit, model and view setup

--- src/lib/game/unit.js
+++ src/lib/game/unit.js
@@ -13,16 +13,24 @@ class Unit extends Entity {
     this.level = '?';
     this.target = null;
 
-    this.position = new THREE.Vector3();
-
     this.maxHp = 0;
     this.hp = 0;
 
     this.maxMp = 0;
     this.mp = 0;
 
-    this.displayID = 0;
-    this.model = null;
+    this._view = new THREE.Group();
+
+    this._displayID = 0;
+    this._model = null;
+  }
+
+  get position() {
+    return this._view.position;
+  }
+
+  get displayID() {
+    return this._displayID;
   }
 
   set displayID(displayID) {
@@ -31,6 +39,7 @@ class Unit extends Entity {
     }
 
     DBC.load('CreatureDisplayInfo', displayID).then((displayInfo) => {
+      this._displayID = displayID;
       this.displayInfo = displayInfo;
       const { modelID } = displayInfo;
 
@@ -40,16 +49,69 @@ class Unit extends Entity {
         this.displayInfo.modelData = this.modelData;
 
         M2.load(this.modelData.file).then((m2) => {
-          // TODO: Clone M2 instance to avoid modifying cached version
-          this.emit('change:model', this.model, m2);
+          m2.displayInfo = this.displayInfo;
           this.model = m2;
-          this.model.displayInfo = this.displayInfo;
-
-          // TODO: Unit should become a THREE.Group at some point
-          this.model.position.copy(this.position);
         });
       });
     });
+  }
+
+  get view() {
+    return this._view;
+  }
+
+  get model() {
+    return this._model;
+  }
+
+  set model(m2) {
+    // TODO: Should this support multiple models? Mounts?
+    if (this._model) {
+      this.view.remove(this._model);
+    }
+    this.view.add(m2);
+    this.emit('model:change', this, this._model, m2);
+    this._model = m2;
+  }
+
+  ascend(distance) {
+    this.view.translateZ(distance);
+    this.emit('position:change', this);
+  }
+
+  descend(distance) {
+    this.view.translateZ(-distance);
+    this.emit('position:change', this);
+  }
+
+  moveForward(distance) {
+    this.view.translateX(distance);
+    this.emit('position:change', this);
+  }
+
+  moveBackward(distance) {
+    this.view.translateX(-distance);
+    this.emit('position:change', this);
+  }
+
+  rotateLeft(angle) {
+    this.view.rotateZ(angle);
+    this.emit('position:change', this);
+  }
+
+  rotateRight(angle) {
+    this.view.rotateZ(-angle);
+    this.emit('position:change', this);
+  }
+
+  strafeLeft(distance) {
+    this.view.translateY(distance);
+    this.emit('position:change', this);
+  }
+
+  strafeRight(distance) {
+    this.view.translateY(-distance);
+    this.emit('position:change', this);
   }
 
 }
