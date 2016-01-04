94b3a3cc1aedbbc6f2d83c3da1ff6ad2433db65b
Tim Kurvers <tim@moonsphere.net>
2016-01-04 01:19:40 +0100

    Drop chat and quests panel for now

--- src/components/game/hud/index.jsx
+++ src/components/game/hud/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './index.styl';
 
-import Chat from '../chat';
+// TODO: import Chat from '../chat';
 import Portrait from '../portrait';
-import Quests from '../quests';
+// TODO: import Quests from '../quests';
 import session from '../../wowser/session';
 
 class HUD extends React.Component {
@@ -15,9 +15,6 @@ class HUD extends React.Component {
       <hud className="hud">
         <Portrait self unit={ player } />
         { player.target && <Portrait target unit={ player.target } /> }
-
-        <Chat />
-        <Quests />
       </hud>
     );
   }
