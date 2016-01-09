183d7adcaf38f2781cd0b251e39de03825199d56
Tim Kurvers <tim@moonsphere.net>
2016-01-09 23:41:53 +0100

    Simplify server

--- src/lib/server/index.js
+++ src/lib/server/index.js
@@ -1,74 +1,24 @@
-import cluster from 'cluster';
-import os from 'os';
 import express from 'express';
 import logger from 'morgan';
 
 import Pipeline from './pipeline';
-import ServerConfig from './utils/server-config';
 
 class Server {
 
-  constructor(root = __dirname) {
-    this.isFirstRun = ServerConfig.db.get('isFirstRun');
+  constructor(port, root = process.pwd) {
+    this.port = port;
     this.root = root;
+
+    this.app = express();
+
+    this.app.set('root', this.root);
+    this.app.use(logger('dev'));
+    this.app.use(express.static('./public'));
+    this.app.use('/pipeline', new Pipeline().router);
   }
 
   start() {
-    if (this.isFirstRun) {
-      ServerConfig
-        .prompt()
-        .then(resultMsg => {
-          console.log(resultMsg);
-          this.run();
-        });
-    } else {
-      this.run();
-    }
-  }
-
-  createApp() {
-    const app = express();
-
-    app.set('root', this.root);
-    app.use(logger('dev'));
-    app.use(express.static('./public'));
-    app.use('/pipeline', new Pipeline().router);
-
-    return app;
-  }
-
-  run() {
-    const serverPort = parseInt(ServerConfig.db.get('serverPort'), 10);
-
-    if (cluster.isMaster) {
-      console.log(`> Settings loaded from ${ServerConfig.db.path}\n` +
-                  "> Use 'npm run reset' to clear settings\n");
-
-      console.log(`> Starting server at localhost:${serverPort}`);
-
-      this.runMaster();
-    } else {
-      this.runWorker(serverPort);
-    }
-  }
-
-  runMaster() {
-    console.log(`> Spawning master`);
-
-    // 1 worker per CPU, up to 4 workers in total
-    const workerCount = Math.min(os.cpus().length, 4);
-
-    // Fork workers
-    for (let i = 0; i < workerCount; ++i) {
-      cluster.fork();
-    }
-  }
-
-  runWorker(serverPort) {
-    console.log(`> Spawning worker (#${cluster.worker.id})`);
-
-    this.app = this.createApp();
-    this.app.listen(serverPort);
+    this.app.listen(this.port);
   }
 
 }
