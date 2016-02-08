9976002247bfd36c67098628bea6fac461a980b6
fallenoak <fallenoak@uh-oh.co>
2016-02-07 19:34:52 -0600

    Only show map stats if a map is loaded
    Also added map chunks stat to map stats.

--- src/components/game/stats/index.jsx
+++ src/components/game/stats/index.jsx
@@ -9,6 +9,59 @@ class Stats extends React.Component {
     map: React.PropTypes.object
   };
 
+  mapStats() {
+    const map = this.props.map;
+
+    return (
+      <div>
+        <div className="divider"></div>
+
+        <h2>Map Chunks</h2>
+        <div className="divider"></div>
+        <p>
+          Loaded: { map ? map.chunks.size : 0 }
+        </p>
+
+        <div className="divider"></div>
+
+        <h2>Map Doodads</h2>
+        <div className="divider"></div>
+        <p>
+          Loading: { map ? map.doodadManager.entriesPendingLoad.size : 0 }
+        </p>
+        <p>
+          Loaded: { map ? map.doodadManager.doodads.size : 0 }
+        </p>
+        <p>
+          Animated: { map ? map.doodadManager.animatedDoodads.size : 0 }
+        </p>
+
+        <div className="divider"></div>
+
+        <h2>WMOs</h2>
+        <div className="divider"></div>
+        <p>
+          Loading Roots: { map ? map.wmoManager.entriesPendingLoad.size : 0 }
+        </p>
+        <p>
+          Loaded Roots: { map ? map.wmoManager.wmos.size : 0 }
+        </p>
+        <p>
+          Loading Groups: { map ? map.wmoManager.groupsPendingLoadCount : 0 }
+        </p>
+        <p>
+          Loaded Groups: { map ? map.wmoManager.groupCount : 0 }
+        </p>
+        <p>
+          Loading Doodads: { map ? map.wmoManager.doodadsPendingLoadCount : 0 }
+        </p>
+        <p>
+          Loaded Doodads: { map ? map.wmoManager.doodadCount : 0 }
+        </p>
+      </div>
+    );
+  }
+
   render() {
     const renderer = this.props.renderer;
     if (!renderer) {
@@ -49,42 +102,7 @@ class Stats extends React.Component {
           Vertices: { render.vertices }
         </p>
 
-        <div className="divider"></div>
-
-        <h2>Map Doodads</h2>
-        <div className="divider"></div>
-        <p>
-          Loading: { map ? map.doodadManager.entriesPendingLoad.size : 0 }
-        </p>
-        <p>
-          Loaded: { map ? map.doodadManager.doodads.size : 0 }
-        </p>
-        <p>
-          Animated: { map ? map.doodadManager.animatedDoodads.size : 0 }
-        </p>
-
-        <div className="divider"></div>
-
-        <h2>WMOs</h2>
-        <div className="divider"></div>
-        <p>
-          Loading Roots: { map ? map.wmoManager.entriesPendingLoad.size : 0 }
-        </p>
-        <p>
-          Loaded Roots: { map ? map.wmoManager.wmos.size : 0 }
-        </p>
-        <p>
-          Loading Groups: { map ? map.wmoManager.groupsPendingLoadCount : 0 }
-        </p>
-        <p>
-          Loaded Groups: { map ? map.wmoManager.groupCount : 0 }
-        </p>
-        <p>
-          Loading Doodads: { map ? map.wmoManager.doodadsPendingLoadCount : 0 }
-        </p>
-        <p>
-          Loaded Doodads: { map ? map.wmoManager.doodadCount : 0 }
-        </p>
+        { map && this.mapStats() }
       </stats>
     );
   }
