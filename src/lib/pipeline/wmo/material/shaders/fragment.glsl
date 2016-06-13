6abb59414a46404e9201ea79e07d512f6ed4660b
fallenoak <github@fallenoak.me>
2016-06-12 22:51:05 -0500

    Synced WMO material fragment shader with latest changes
    Also added an ifdef to still permit rendering without world lighting.

--- src/lib/pipeline/wmo/material/shaders/fragment.glsl
+++ src/lib/pipeline/wmo/material/shaders/fragment.glsl
@@ -15,26 +15,35 @@ void main() {
   vec4 color = texture2D(textures[0], vCoord0);
 
   // Lighting
-  #ifdef BATCH_TYPE
-    #if BATCH_TYPE == 1
-      color.rgb *= mix(vColor0.rgb, vWorldLight.rgb, vColor0.a);
-    #endif
+  #ifdef USE_WORLD_LIGHT
+    #ifdef BATCH_TYPE
+      #if BATCH_TYPE == 1
+        color.rgb *= mix(vColor0.rgb * 2.0, vWorldLight.rgb, vColor0.a);
+      #endif
 
-    #if BATCH_TYPE == 2
-      color.rgb *= mix(vColor0.rgb, vWorldLight.rgb + vColor0.rgb, vColor0.a);
-    #endif
+      #if BATCH_TYPE == 2
+        color.rgb *= mix(vColor0.rgb * 2.0, vWorldLight.rgb + (vColor0.rgb * 2.0), vColor0.a);
+      #endif
 
-    #if BATCH_TYPE == 3
-      color.rgb *= mix(vColor0.rgb, vWorldLight.rgb, 1.0);
+      #if BATCH_TYPE == 3
+        color.rgb *= mix(vColor0.rgb * 2.0, vWorldLight.rgb, 1.0);
+      #endif
+    #else
+      color.rgb *= mix(vColor0.rgb * 2.0, vWorldLight.rgb, 1.0);
     #endif
-  #else
-    color.rgb *= mix(vColor0.rgb, vWorldLight.rgb, 1.0);
   #endif
 
   // Fog
   color.rgb = mix(color.rgb, fogColor.rgb, vFogFactor);
 
+  // Clean up
   #ifdef BLENDING_MODE
+    // Force transparent pixels to opaque in blending mode 0
+    #if BLENDING_MODE == 0
+      color.a = 1.0;
+    #endif
+
+    // Scale pixel transparency with fog factor in blending mode 1
     #if BLENDING_MODE == 1
       color.a = mix(color.a, 1.0, vFogFactor);
     #endif
