d70f989c486706975b14ad47ec2ffcd60649b89b
fallenoak <github@fallenoak.me>
2016-08-20 20:16:28 -0500

    Initial implementation of vertex color attenuation in WMO groups

--- src/lib/pipeline/wmo/group/index.js
+++ src/lib/pipeline/wmo/group/index.js
@@ -1,5 +1,6 @@
 import THREE from 'three';
 
+import WMORootFlags from '../root/flags';
 import WMOGroupView from './view';
 import BSPTree from '../../../utils/bsp-tree';
 
@@ -17,6 +18,7 @@ class WMOGroup {
 
     this.createPortals(root, def);
     this.createMaterial(def.materialRefs);
+    this.attenuateVertexColors(root, def.attributes, def.batches);
     this.createGeometry(def.attributes, def.batches);
     this.createBoundingBox(def.boundingBox);
     this.createBSPTree(def.bspNodes, def.bspPlaneIndices, def.attributes);
@@ -150,6 +152,87 @@ class WMOGroup {
     }
 
     return (result.portal === null) ? null : result;
+  }
+
+  attenuateVertexColors(root, attributes, batches) {
+    if (root.header.flags & WMORootFlags.SKIP_MOCV_ATTENUATION) {
+      return;
+    }
+
+    const { batchCounts, batchOffsets } = this.header;
+
+    if (batchCounts.a === 0) {
+      return;
+    }
+
+    const firstBatchB = batches[batchOffsets.b];
+
+    const vertices = attributes.positions;
+    const colors = attributes.colors;
+
+    const vmax = firstBatchB ? firstBatchB.firstVertex : vertices.length;
+
+    for (let vindex = 0; vindex < vmax; ++vindex) {
+      const color = colors.subarray(vindex * 4, vindex * 4 + 4);
+      const vertex = vertices.subarray(vindex * 3, vindex * 3 + 3);
+
+      // In the case of no portals, there is no world light
+      if (this.portals.length === 0) {
+        color[3] = 0.0;
+        continue;
+      }
+
+      const origin = new THREE.Vector3(vertex[0], vertex[1], vertex[2]);
+      const closestPortal = this.closestPortal(origin, 6.0);
+
+      if (!closestPortal) {
+        color[3] = 0.0;
+        continue;
+      }
+
+      let attenuation = 0.0;
+      let newAlpha = 0.0;
+
+      const distance = closestPortal.distance;
+
+      const destinationFlags = root.groupInfo[closestPortal.portalRef.groupIndex].flags;
+
+      if (destinationFlags & (0x08 | 0x40)) {
+        if (distance < 0.0) {
+          attenuation = 1.0;
+        } else {
+          attenuation = 1.0 - (distance / 6.0);
+        }
+      }
+
+      if (attenuation <= 0.001) {
+        attenuation = 0.0;
+        newAlpha = 0.0;
+      } else if (attenuation <= 1.0) {
+        newAlpha = attenuation * 255.0;
+      } else {
+        attenuation = 1.0;
+        newAlpha = 255.0;
+      }
+
+      // Red
+      const tempR = color[0] * 255.0;
+      const newR = ((127.0 - tempR) * attenuation) + tempR;
+      color[0] = newR / 255.0;
+
+      // Green
+      const tempG = color[1] * 255.0;
+      const newG = ((127.0 - tempG) * attenuation) + tempG;
+      color[1] = newG / 255.0;
+
+      // Blue
+      const tempB = color[2] * 255.0;
+      const newB = ((127.0 - tempB) * attenuation) + tempB;
+      color[2] = newB / 255.0;
+
+      // Alpha
+      color[3] = newAlpha / 255.0;
+    }
   }
 
 }
